16aa2d7fbc64e57dc8b81ae34d79c84e0493a7a6
Izac-Silva <izaclislanio@gmail.com>
2022-04-20 07:23:24 -0300

    learning async code

--- assets/service/cliente-service.js
+++ assets/service/cliente-service.js
@@ -1,3 +1,25 @@
+const criaNovaLinha = (nome, email) => {
+    const novaLinha = document.createElement('tr')
+
+    const conteudo = 
+        `
+            <td class="td" data-td>${nome}</td>
+            <td>${email}</td>
+            <td>
+                <ul class="tabela__botoes-controle">
+                    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                </ul>
+            </td>
+        `
+
+        novaLinha.innerHTML = conteudo
+
+        return novaLinha
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
 const http = new XMLHttpRequest()
 
 http.open("GET", "http://localhost:3000/profile")
@@ -5,7 +27,12 @@ http.open("GET", "http://localhost:3000/profile")
 http.send()
 
 http.onload = () => {
-    const data = http.response
 
-    console.log(data)
+    const data = JSON.parse(http.response)
+    
+    data.forEach(element => {
+        tabela.appendChild(criaNovaLinha(element.nome, element.email))
+    
+
+    });
 } 
